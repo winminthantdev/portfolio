450bbc050db02237f19727bd66cc84344d4dec25
winminthant <winminthant.gwin@gmail.com>
2025-09-25 00:56:17 +0630

    projectsection

--- frontend/src/components/Project/Project.jsx
+++ frontend/src/components/Project/Project.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./Project.css"
 import { motion } from "framer-motion"
 import { projects } from '../../assets/assets';
 import { PiGithubLogoLight,PiShare } from 'react-icons/pi';
@@ -11,22 +12,21 @@ const Project = () => {
 
         {
           projects.map((project, index)=>(    
-            <motion.div key={project.id} initial={{ opacity: 0}} whileInView={{ opacity: 1 }} viewport={{ once: true }} transition={{ duration: 0.6, ease:"easeOut", delay: index * 0.1 }} className="group border border-gray-400 rounded overflow-hidden backdrop-blur-[2px] hover:backdrop-blur-[4px] rounded-lg transition">
+            <motion.div key={project.id} initial={{ opacity: 0}} whileInView={{ opacity: 1 }} viewport={{ once: true }} transition={{ duration: 0.6, ease:"easeOut", delay: index * 0.1 }} className="group border border-gray-400 rounded overflow-hidden backdrop-blur-[2px] hover:backdrop-blur-[4px] rounded-lg transition myanis">
               <div className="w-full relative h-60 bg-white overflow-hidden">
                 <img src={project.photo} className='w-full h-full group-hover:scale-110 transition duration-500 object-contain' alt="" />
                 <div className="w-full h-full absolute left-0 bottom-0 group-hover:bg-black/30 transition duration-500">
-                  <div className="w-full h-full translate-y-[100%] group-hover:translate-y-0 transition duration-500">
-                        <div className="w-full h-full flex justify-center items-center gap-2">
-                          <button className='flex justify-center items-center gap-1 bg-black rounded px-4 py-1'>
+                  <div className="w-full h-full flex justify-center items-center gap-2">
+                          <button className='flex justify-center items-center gap-1 bg-black rounded cursor-pointer px-4 py-1 translate-y-[100%] group-hover:translate-y-0 transition duration-500 utpani1'>
                             <PiShare size={18}  />
-                            <a href={project.link} className=''>Live Demo</a>
+                            <a href={project.link} target='_blank' className=''>Live Demo</a>
                           </button>
-                          <button className='flex justify-center items-center gap-1 bg-black rounded px-4 py-1'>
+                          <button className='flex justify-center items-center gap-1 bg-black rounded cursor-pointer px-4 py-1 utpani2'>
                             <PiGithubLogoLight size={18} />
-                            <a href={project.gitlink} className='inline-block'>Source</a>
+                            <a href={project.gitlink} target='_blank' className='inline-block'>Source</a>
                           </button>
-                        </div>
                   </div>
+                  
                 </div>
               </div>
               <div className="p-4">
